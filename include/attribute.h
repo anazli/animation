773d93e7e603d29822e56323e3ad641399653cd1
anazli <andreas221b@gmail.com>
2025-12-28 09:17:44 +0100

    update attribute class

--- include/attribute.h
+++ include/attribute.h
@@ -2,6 +2,8 @@
 
 #include <vector>
 
+#include "glad/glad.h"
+
 namespace core {
 template <typename T>
 class Attribute {
@@ -22,4 +24,54 @@ class Attribute {
   unsigned int m_handle;
   unsigned int m_data_count;
 };
+
+template <typename T>
+inline core::Attribute<T>::Attribute() : m_data_count(0) {
+  glGenBuffers(1, &m_handle);
+}
+
+template <typename T>
+core::Attribute<T>::~Attribute() {
+  glDeleteBuffers(1, &m_handle);
+}
+
+template <typename T>
+inline void Attribute<T>::SetData(T* input, unsigned int length) {
+  m_data_count = length;
+  unsigned int size = sizeof(T);
+  glBindBuffer(GL_ARRAY_BUFFER, m_handle);
+  glBufferData(GL_ARRAY_BUFFER, size * m_data_count, input, GL_STREAM_DRAW);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+template <typename T>
+inline void Attribute<T>::SetData(std::vector<T>& input) {
+  SetData(&input[0], static_cast<unsigned int>(input.size()));
+}
+
+template <typename T>
+inline void Attribute<T>::BindTo(unsigned int slot) {
+  glBindBuffer(GL_ARRAY_BUFFER, m_handle);
+  glEnableVertexAttribArray(slot);
+  SetAttribPointer(slot);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+template <typename T>
+inline void Attribute<T>::UnbindFrom(unsigned int slot) {
+  glBindBuffer(GL_ARRAY_BUFFER, m_handle);
+  glDisableVertexAttribArray(slot);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+template <typename T>
+inline unsigned int Attribute<T>::GetHandle() const {
+  return m_handle;
+}
+
+template <typename T>
+inline unsigned int Attribute<T>::GetDataCount() const {
+  return m_data_count;
+}
+
 }  // namespace core
