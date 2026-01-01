0ea5d0365c019d53b9f923df853fbd8961f03f3e
anazli <andreas221b@gmail.com>
2026-01-01 09:31:47 +0100

    update quat class

--- include/math/constants.h
+++ include/math/constants.h
@@ -4,3 +4,4 @@
 
 const float PI = acos(-1.);
 const float RAD = 360.f;
+constexpr float EPS = 0.0001f;
--- include/math/quat.h
+++ include/math/quat.h
@@ -2,6 +2,7 @@
 
 #include <cmath>
 
+#include "math/constants.h"
 #include "math/vec3.h"
 
 class Quat {
@@ -9,8 +10,19 @@ class Quat {
   Quat();
   Quat(float x, float y, float z, float w);
 
+  float x() const { return m_x; }
+  float y() const { return m_y; }
+  float z() const { return m_z; }
+  float w() const { return m_w; }
+
+  float squared_length() const;
+  float length() const;
+
   Quat angle_axis(float angle, const Vec3f& axis);
   Quat from(const Vec3f& from, const Vec3f& to);
+
+  Vec3f get_axis(const Quat& quat);
+  float get_angle(const Quat& quat);
 
  private:
   float m_x;
@@ -23,6 +35,48 @@ inline Quat::Quat() : m_x(0.f), m_y(0.f), m_z(0.f), m_w(1.f) {}
 
 inline Quat::Quat(float x, float y, float z, float w)
     : m_x(x), m_y(y), m_z(z), m_w(w) {}
+
+inline Quat operator+(const Quat& q1, const Quat& q2) {
+  return Quat(q1.x() + q2.x(), q1.y() + q2.y(), q1.z() + q2.z(),
+              q1.w() + q2.w());
+}
+
+inline Quat operator-(const Quat& q1, const Quat& q2) {
+  return Quat(q1.x() - q2.x(), q1.y() - q2.y(), q1.z() - q2.z(),
+              q1.w() - q2.w());
+}
+
+inline Quat operator*(const Quat& q1, float n) {
+  return Quat(q1.x() * n, q1.y() * n, q1.z() * n, q1.w() * n);
+}
+
+inline Quat operator-(const Quat& q) {
+  return Quat(-q.x(), -q.y(), -q.z(), -q.w());
+}
+
+inline bool operator==(const Quat& q1, const Quat& q2) {
+  return (fabsf(q1.x() - q2.x()) <= EPS && fabsf(q1.y() - q2.y()) <= EPS &&
+          fabsf(q1.z() - q2.z()) <= EPS && fabsf(q1.w() - q2.w()) <= EPS);
+}
+
+bool operator==(const Quat& q1, const Quat& q2) { return !(q1 == q2); }
+
+bool same_orientation(const Quat& q1, const Quat& q2) {
+  return (fabsf(q1.x() - q2.x()) <= EPS && fabsf(q1.y() - q2.y()) <= EPS &&
+          fabsf(q1.z() - q2.z()) <= EPS && fabsf(q1.z() - q2.z()) <= EPS) ||
+         (fabsf(q1.x() + q2.x()) <= EPS && fabsf(q1.y() + q2.y()) <= EPS &&
+          fabsf(q1.z() + q2.z()) <= EPS && fabsf(q1.z() + q2.z()) <= EPS);
+}
+
+float dot(const Quat& q1, const Quat& q2) {
+  return q1.x() + q2.x() + q1.y() + q2.y() + q1.z() + q2.z() + q1.w() + q2.w();
+}
+
+inline float Quat::squared_length() const {
+  return x() * x() + y() * y() + z() * z() + w() * w();
+}
+
+inline float Quat::length() const { return sqrtf(squared_length()); }
 
 inline Quat Quat::angle_axis(float angle, const Vec3f& axis) {
   Vec3f norm(axis);
@@ -58,3 +112,11 @@ inline Quat Quat::from(const Vec3f& from, const Vec3f& to) {
   auto axis = cross(f, half);
   return Quat(axis.x(), axis.y(), axis.z(), dot(f, half));
 }
+
+inline Vec3f Quat::get_axis(const Quat& quat) {
+  Vec3f v(quat.x(), quat.y(), quat.z());
+  v.normalize();
+  return v;
+}
+
+inline float Quat::get_angle(const Quat& quat) { return 2.f * acosf(quat.w()); }
